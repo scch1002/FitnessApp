39054fe384c1b79cb9f3f6855724a1e553d94c92
scch1002 <chrisschwan8009@hotmail.com>
2017-09-21 20:43:56 -0500

    Finished editing views

---
+++ WorkOut.App.Forms/Converter/BooleanToCompleteConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WorkOut.App.Forms.Converter
+{
+    public class BooleanToCompleteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var complete = value as bool?;
+
+            return complete.HasValue && complete.Value ? "Complete" : "Not Complete";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
